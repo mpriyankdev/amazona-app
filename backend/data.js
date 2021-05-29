0307d6a75000a487881cec131c49557cc1d9d6d1
Priyank Maheshwari <mpriyank.dev@gmail.com>
2021-05-29 15:33:57 +0530

    basic backend code

---
+++ backend/data.js
@@ -0,0 +1,80 @@
+
+
+const data = {
+    products:[
+        {
+            _id:'1',
+            name:'Nike Slim Shirt',
+            category:'Shirts',
+            image:'/images/p1.jpg',
+            brand:'Nike',
+            rating:4.5,
+            numReviews:10,
+            price:100,
+            countInStock:10,
+            description:'high quality product'
+        },
+        {
+            _id:'2',
+            name:'Adidas Slim Shirt',
+            category:'Shirts',
+            image:'/images/p2.jpg',
+            brand:'Adidas',
+            rating:4,
+            numReviews:100,
+            price:150,
+            countInStock:20,
+            description:'high quality product'
+        },
+        {
+            _id:'3',
+            name:'Lacose Shirt',
+            category:'Shirts',
+            image:'/images/p3.jpg',
+            brand:'Lacoste',
+            rating:4.5,
+            numReviews:150,
+            price:100,
+            countInStock:0,
+            description:'high quality product'
+        },
+        {
+            _id:'4',
+            name:'Nike Slim Pant',
+            category:'Pants',
+            image:'/images/p4.jpg',
+            brand:'Nike',
+            rating:4.5,
+            numReviews:10,
+            price:100,
+            countInStock:15,
+            description:'high quality product'
+        },
+        {
+            _id:'5',
+            name:'Puma Slim Pant',
+            category:'Pants',
+            image:'/images/p5.jpg',
+            brand:'Puma',
+            rating:5,
+            numReviews:200,
+            price:100,
+            countInStock:10,
+            description:'high quality product'
+        },
+        {
+            _id:'6',
+            name:'Adidas Fit Pants',
+            category:'Pants',
+            image:'/images/p6.jpg',
+            brand:'Adidas',
+            rating:4.5,
+            numReviews:10,
+            price:100,
+            countInStock:5,
+            description:'high quality product'
+        }
+    ]
+};
+
+export default data;
